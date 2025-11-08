c2fe3ebf600fc5eb328ccb4e6607088063c481c5
princetechbehind <prince.techbehind@gmail.com>
2025-11-08 16:33:56 +0530

    header and Hero section UI completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,33 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+
+            // ✅ Custom Colors
+            colors: {
+                primary: {
+                    DEFAULT: '#0FA64B',
+                    light: '#1ad564',
+                    dark: '#0b7535',   
+                },
+                grey: {
+                    DEFAULT: '#aeb0b5', // purple-600
+                    light: '#E7E9F5',   // purple-400
+                    dark: '#6c6d70',    // purple-700
+                },
+                accent: '#F59E0B', // amber-500
+                neutral: '#374151', // gray-700
+            },
+
+            // ✅ Custom Breakpoints (screens)
+            screens: {
+                xs: '480px',
+                sm: '640px',
+                md: '768px',
+                lg: '1024px',
+                xl: '1280px',
+                '2xl': '1536px',
+                '3xl': '1920px', // Extra large custom breakpoint
+            },
         },
     },
 

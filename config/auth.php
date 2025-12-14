63efafc9559596cf4223b8b9163c3c5bc193bf26
= <dawinder.twotechies@gmail.com>
2025-12-14 18:43:12 +0530

    changes revert

--- config/auth.php
+++ config/auth.php
@@ -3,14 +3,13 @@
 return [
 
     /*
-    
     |--------------------------------------------------------------------------
     | Authentication Defaults
     |--------------------------------------------------------------------------
     |
     | This option defines the default authentication "guard" and password
     | reset "broker" for your application. You may change these values
-    | as required, but they're a perfect start for most applications.
+    | as required, but they 're a perfect start for most applications.
     |
     */
 
